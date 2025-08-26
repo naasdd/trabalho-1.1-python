61c025fd2e8a670f457700959a923e456f32c592
Jose Gabriel <josegabrielg2014@gmail.com>
2025-08-26 14:09:06 -0300

    Ex 1 terminado

--- exercicios/ex1.py
+++ exercicios/ex1.py
@@ -1,10 +1,28 @@
-print("Hello world")
-
 valor = int(input("Insira um numero: "))
 
 fibonacci = [0, 1]
 
 print("   ")
 
+i = 2
+while True:
+    novoValor = (fibonacci[i - 1] + fibonacci[i - 2])
+    fibonacci.append(novoValor)
+    if (novoValor > valor):
+        menor = fibonacci[i] - valor
+        maior = valor - fibonacci[i - 1]
 
+        if (maior > menor):
+            resposta = fibonacci[i]
+            print("resposta = fibonacci[i] = ", fibonacci[i])
+        else:
+            resposta = fibonacci[i - 1]
+            print("resposta = fibonacci[i - 1] = ", fibonacci[i - 1])
 
+        break
+    else:
+        i = i + 1
+
+print(fibonacci)
+
+print("O valor da sequência de fibonacci mais próximo de", valor, "é: ", resposta)
